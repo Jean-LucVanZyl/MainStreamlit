a031e930b05e1ec1fb864d2dfccedbf6ae2f3eec
Jean-LucVanZyl <81359070+Jean-LucVanZyl@users.noreply.github.com>
2021-06-24 16:12:05 +0200

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -34,6 +34,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Our dependencies
+from PIL import Image
 import nltk
 import seaborn as sns
 import re
@@ -158,8 +159,8 @@ def main():
         
         st.info('This page contains various key data insights that guided our Exploration of our data, and the factors of data preprocessing and visualisations that we utilised. The visualisations may be enlarged by clicking the icon at the top right corner of it. ')
         from PIL import Image
-        image = Image.open('Images/format_of_data.png')
-        st.image(image, caption='')
+        image1 = Image.open('Images/format_of_data.png')
+        st.image(image1, caption='')
         st.write("This is the format our data is in. We have messages, its respective tweet ID as well as the message's sentiment with regards to climate change. There are 4 sentiment expressions, namely;")
         st.write("* 1 Pro: The tweet supports the belief of our man made impact on climate change. ")
         st.write("* 2 News: the tweet links to factual news about climate change.")
@@ -167,22 +168,22 @@ def main():
         st.write("* 1 Anti: the tweet does not believe in man-made climate change.")
 
         from PIL import Image
-        image = Image.open('Images/dis_classes.png')
-        st.image(image, caption='')
+        image2 = Image.open('Images/dis_classes.png')
+        st.image(image2, caption='')
         st.write("From the figure above, we observed that we have unbalanced classes. * The majority of tweets (53.9%) support the belief of man-made climate change. * 23% consist of factual news regarding climate change. * 14.9% are neutral about man-made climate change* 8.2% don't believe in man-made climate change")
 
         st.write(" ")
         st.write(" ")
         st.write("Lets have a look at the count distribution between the different sentiment expressions labeled respectively.")
         from PIL import Image
-        image = Image.open('Images/bar_sent.png')
-        st.image(image, caption='Count of sentiment expressions')
+        image3 = Image.open('Images/bar_sent.png')
+        st.image(image3, caption='Count of sentiment expressions')
 
         st.write("  Next, lets investigate into the number of unique words used in each class.")
 
         from PIL import Image
-        image = Image.open('Images/box_plot.png')
-        st.image(image, caption='Number of words for corresponding sentiment class')
+        image4 = Image.open('Images/box_plot.png')
+        st.image(image4, caption='Number of words for corresponding sentiment class')
 
         st.write("Tweets representing news contain less words. People who believe in man-made climate change appear to used on average the same ammount of words")
 
@@ -190,8 +191,8 @@ def main():
         st.write("* First we obtained a list containing all the words. * Afterwards we obtained the lenth of each word and counted the number of times the word appears in our list. * Lastly we grouped frequencies by lenght and summed them up.")
 
         from PIL import Image
-        image = Image.open('Images/word_length.png')
-        st.image(image, caption='')
+        image5 = Image.open('Images/word_length.png')
+        st.image(image5, caption='')
 
         st.write("The lengths of the words ranged from 1-70, to obtain a better visualisation we limited the domain to words of lengths 1-20. The length of the words appears to be positively skewed. We can expect the data to contain outliers to the right of the distribution. Most words lengths (78.4%) lies between 3-8,with the peak being 7.")
 
@@ -199,8 +200,8 @@ def main():
 
         st.write("As our dataframe consists of messages shared between users on Twitter, we can assume that people would share various websites containing information they feel strongly about, mention specific people or keywords and whatever may be trending at this current point in time. Lets explore the frequency first with respect to the above. ")
         from PIL import Image
-        image = Image.open('Images/freq.png')
-        st.image(image, caption='Frequencies of Hashtags, Mentions, URL links and Retweets')
+        image6 = Image.open('Images/freq.png')
+        st.image(image6, caption='Frequencies of Hashtags, Mentions, URL links and Retweets')
 
 
 
@@ -208,23 +209,23 @@ def main():
 
         st.write("We'll explore the specifics in more detail, below are wordclouds where the size of the words represents the frequency of use for the particular class. ")
         from PIL import Image
-        image = Image.open('Images/Wordclouds.png')
-        st.image(image, caption='')
+        image7 = Image.open('Images/Wordclouds.png')
+        st.image(image7, caption='')
         st.write("Climate change and global warming are two of the most common words that appear in our dataset. This is not surprising as the dataset is collected on the tweets based on these terms https is another popular word found in the tweet. This suggests that many tweets contain web links that may address the climate change topics and the widely shared among the general publicNames of the controversial figures appear commonly in the tweets. The former president of the United States, Donald Trump and the Administrator of the U.SEnvironmental Protection Agency (EPA), Scott Pruitt are well-known for their rejection of scientific consensus on climate change. Contrastingly, senator Bernie Sanders, Al Gore and Leo DiCaprio are famous activist/advocates for man-made climate change.Terms such as Republican, EPA, Chinese, America occur frequently in the tweets, indicating that climate change is perceived as a political issue")
 
         st.subheader("Mentions and Hashtags, the key ingredients for what is important to the population at any point in time.")
         st.info("Images can be enlarged at the upper right corner of said visualization.")
         from PIL import Image
-        image = Image.open('Images/at.png')
-        st.image(image, caption='')
+        image9 = Image.open('Images/at.png')
+        st.image(image9, caption='')
         from PIL import Image
-        image = Image.open('Images/hashtag.png')
-        st.image(image, caption='')
+        image10 = Image.open('Images/hashtag.png')
+        st.image(image10, caption='')
 
         st.write("If our aim were to be to change the minds of those who do not believe in man made climate change, we can do research on their class sentiment hashtags and mentions, understand their key arguments and structure our response in accordance to their views and beliefs. We as a species are ever changing, and a long held belief may change depending on the emergence of scientific literature, the opinions of political leaders, to whether an article written with factual news(or not) grabs our attention and influences what we believe, and even to a single tweet from an actor.")
         from PIL import Image
-        image = Image.open('Images/polarity.png')
-        st.image(image, caption='Polarity in subjectivity of the classes')
+        image11 = Image.open('Images/polarity.png')
+        st.image(image11, caption='Polarity in subjectivity of the classes')
         st.write("We are so polarized in our opinions, that it is extremely difficult to make progress while only considering the merits of our own beliefs.")
         st.write("With all of the tools above, we can understand the views of our fellow people as the times are changing, structure our marketing, discussions and campaigns with data driven arguments that incorporates the respective views of the population without falling into the trap of forcing confirmity to our views upon them and expecting progress. ")
 
